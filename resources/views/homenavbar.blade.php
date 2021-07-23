b9d5dc7474e66b4be458b824aad7ffc00483d162
Muhammad Ridhuan <wan123jaafar@gmail.com>
2021-07-23 22:21:22 +0800

    Update homenavbar.blade.php

--- resources/views/homenavbar.blade.php
+++ resources/views/homenavbar.blade.php
@@ -1,7 +1,9 @@
 <nav class="navbar navbar-expand-lg navbar-dark bg-secondary">
   <div class=" navbar-collapse" id="navbarNavAltMarkup">
     <div class="navbar-nav mx-auto">
-      <a class="nav-item nav-link active" href="{{ route('home') }}">Home <span class="sr-only">(current)</span></a>
+      @if (Auth::user()->role == "buyer")
+        <a class="nav-item nav-link active" href="{{ route('home') }}">Home <span class="sr-only">(current)</span></a>
+      @endif
       <a class="nav-item nav-link" href="#">Features</a>
       <a class="nav-item nav-link" href="#">Pricing</a>
     </div>
